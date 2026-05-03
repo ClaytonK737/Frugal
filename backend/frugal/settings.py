95f8e771bd002b281d28dd24b9c53e3fcde9c227
CalebA-Codes <chrmcaleb@gmail.com>
2026-05-02 19:16:48 -0500

    Update settings.py

--- backend/frugal/settings.py
+++ backend/frugal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+KROGER_CLIENT_ID     = os.environ.get('KROGER_CLIENT_ID', '')
+KROGER_CLIENT_SECRET = os.environ.get('KROGER_CLIENT_SECRET', '')
